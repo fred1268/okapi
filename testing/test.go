a9ed990ff717f18fbbeca6a9bad6b9626f2ad31f
Fred <fred@satelia.eu>
2023-07-21 18:35:10 +0200

    Refactoring and improve @file

--- testing/test.go
+++ testing/test.go
@@ -1,15 +1,10 @@
 package testing
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"os"
-	"path"
 	"runtime"
-	"strings"
 	"sync"
 	"time"
 
@@ -31,102 +26,6 @@ type testOut struct {
 	start     time.Time
 	fail      bool
 	logs      []string
-}
-
-func readServersConfigs(filename string) (map[string]*ServerConfig, error) {
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		return nil, fmt.Errorf("file '%s' does not exist: %w", filename, err)
-	}
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("cannot open file '%s': %w", filename, err)
-	}
-	var config map[string]*ServerConfig
-	if err = json.NewDecoder(bytes.NewReader(content)).Decode(&config); err != nil {
-		return nil, fmt.Errorf("cannot decode file '%s': %w", filename, err)
-	}
-	return config, nil
-}
-
-func readExpectedJSON(directory string, requests []*APIRequest) error {
-	for _, request := range requests {
-		if err := request.validate(); err != nil {
-			return fmt.Errorf("invalid test '%s': %w", request.Name, err)
-		}
-		if request.Expected.Response != "@file" {
-			continue
-		}
-		file := fmt.Sprintf("%s.expected.json", strings.ToLower(request.Name))
-		content, err := os.ReadFile(path.Join(directory, file))
-		if err != nil {
-			return fmt.Errorf("cannot read test file '%s': %w", file, err)
-		}
-		request.Expected.Response = string(content)
-	}
-	return nil
-}
-
-// LoadTests reads all test files in the provided directory and
-// returns them sorted by file.
-//
-// The result is a map indexed by the file name, its value being an
-// array of *APIRequests corresponding to the tests in the file.
-func LoadTests(directory string) (map[string][]*APIRequest, error) {
-	files, err := os.ReadDir(directory)
-	if err != nil {
-		return nil, err
-	}
-	allTests := make(map[string][]*APIRequest)
-	for _, file := range files {
-		if !strings.HasSuffix(file.Name(), ".test.json") {
-			continue
-		}
-		content, err := os.ReadFile(path.Join(directory, file.Name()))
-		if err != nil {
-			return nil, fmt.Errorf("cannot read test file '%s': %w", file.Name(), err)
-		}
-		var tests struct {
-			Tests []*APIRequest
-		}
-		if err = json.NewDecoder(bytes.NewReader(content)).Decode(&tests); err != nil {
-			return nil, fmt.Errorf("cannot decode json file '%s': %w", file.Name(), err)
-		}
-		if err := readExpectedJSON(directory, tests.Tests); err != nil {
-			return nil, err
-		}
-		allTests[file.Name()] = tests.Tests
-	}
-	return allTests, nil
-}
-
-// LoadClients reads the configuration file, creates a map of *Client,
-// connects them to their respective servers, and returns it.
-//
-// If something goes wrong during the JSON parsing or HTTP connection,
-// an error will be provided and the map will be nil. The clients can
-// then be used to run tests.
-func LoadClients(ctx context.Context, cfg *Config) (map[string]*Client, error) {
-	serverConfigs, err := readServersConfigs(cfg.Servers)
-	if err != nil {
-		return nil, err
-	}
-	clients := make(map[string]*Client)
-	for key, value := range serverConfigs {
-		client := NewClient(value)
-		if err := client.config.validate(); err != nil {
-			return nil, fmt.Errorf("server %s: invalid configuration: %w", key, err)
-		}
-		if client.config.Auth != nil && client.config.Auth.Login != nil {
-			if apiResponse, err := client.Connect(ctx); err != nil {
-				return nil, fmt.Errorf("cannot connect to server '%s' (response: %v): %w", key, apiResponse, err)
-			}
-			if cfg.Verbose {
-				log.Printf("Connected to %s (%s)\n", key, value.Host)
-			}
-		}
-		clients[key] = client
-	}
-	return clients, nil
 }
 
 func runOne(ctx context.Context, tin *testIn, out chan<- *testOut) error {
